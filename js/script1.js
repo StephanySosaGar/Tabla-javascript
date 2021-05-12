8453cd10eb0e4e156b34c7106fadf85e9abf6353
StephanySosaGarcia <karensosagarciastephany@gmail.com>
2021-05-12 13:41:16 -0500

    first commit

---
+++ js/script1.js
@@ -0,0 +1,12 @@
+for( let i=0;  i<=20; i+=2 )  {
+    let table = `
+        <tr>
+        <th scope="row"> ${i}</th>
+        <td>Luci</td>
+        <td>swan</td>
+        <td>@luswn</td>
+      </tr>`;
+    
+    document.getElementById("tabla")
+    .innerHTML += table; }
+  
